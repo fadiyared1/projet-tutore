c08b01f155b112c8fcc041a37ee2e911be411cf5
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-11-04 15:21:42 +0100

    .

--- shortcodes/identification.php
+++ shortcodes/identification.php
@@ -5,13 +5,19 @@ function identification_shortcode($atts, $content)
 {
     $title = __('Identification', 'personalized-support');
 
-    $content = '<div>
-                    <form method="POST" action="#">
-                        <label for="numero">Numero</label>
-                        <input type="text">
-                        <button type="submit">Login</button>
-                    </form>
-                </div>';
+    $content = "";
+    if (isset($_POST['numeros'])) {
+        $content = $_POST['numeros'];
+    } else {
+        $content = '<div>
+                        <form method="POST" action="">
+                            <label for="numero">Numero</label>
+                            <input type="text" name="numeros">
+                            <button type="submit">Login</button>
+                        </form>
+                    </div>';
+    }
+
 
     return fieldset($title, $content);
 }
