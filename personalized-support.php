cd6600d82f3216e224460eb03df61faaae3837c5
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-11-04 14:35:53 +0100

    .

--- personalized-support.php
+++ personalized-support.php
@@ -24,27 +24,7 @@ function personalized_support_head()
 	wp_enqueue_script('personalized_support_script');
 }
 
-
-add_action('admin_menu', 'personalized_support_menu');
-function personalized_support_menu()
-{
-	add_menu_page(__('Plugin Personalized Support', 'personalized-support'), 'Personalized Support', 'manage_options', 'personalized-support', 'personalized_support_menu_output');
-}
-
-function personalized_support_menu_output()
-{
-	?>
-	<div class="wrap">
-		<h2><?php echo get_admin_page_title() ?></h2>
-
-		<form action="#" method="POST">
-			<label for="numero">Numéros</label>
-			<input type="text" size="50">
-			<button type="submit">Enregistrer les numéros</button>
-		</form>
-	</div>
-<?php
-}
+require_once('admin-page.php');
 
 function fieldset($title_legend, $content)
 {
