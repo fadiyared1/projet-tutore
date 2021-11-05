8c16fd68d880c3ec8b58942c4d974c0834c50068
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-11-05 15:03:01 +0100

    .

--- shortcodes/feedback.php
+++ shortcodes/feedback.php
@@ -6,11 +6,12 @@ function feedback_shortcode($atts, $content)
 	$whole_content = get_the_content();
 
 	$start_metadata_pos = strpos($whole_content, '[meta');
-	$len_metadata = strpos($whole_content, "[/meta]") - $start_metadata_pos + 7;
+	$len_metadata = strpos($whole_content, "[/meta]") - $start_metadata_pos;
 	$metadata_shortcode = substr($whole_content, $start_metadata_pos, $len_metadata);
+	$newstr = substr_replace($metadata_shortcode, "/", $len_metadata - 1, 0);
 
-	$rrrr = shortcode_parse_atts($metadata_shortcode);
-	var_dump($metadata_shortcode);
+	$rrrr = shortcode_parse_atts($newstr);
+	var_dump($newstr);
 	var_dump($rrrr);
 
 	if (PSUser::has_valid_numero())
